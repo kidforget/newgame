a089bd0c2157dad68bc4c375a6b502c642434fb6
FadeFeiPC <gufeicoder@gmail.com>
2017-06-24 15:16:00 +0800

    startUI

---
+++ New Unity Project/Assets/the game/Script/Common/StartUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartUI : MonoBehaviour {
+
+    private GameController game;
+    private Rect winRect = new Rect(10f, 10f, 300f, 100f);
+
+    // Use this for initialization
+    void Start()
+    {
+        game = gameObject.GetComponent<GameController>();
+    }
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width - 350, Screen.height - 400, 400f, 300f), "Start"))
+        {
+            SceneManager.LoadScene("game");
+            //moved = true; Translate(Vector3.right * Time.deltaTime * speed * 10);
+        };
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
